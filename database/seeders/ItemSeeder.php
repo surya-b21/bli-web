750b1c6d706a0bbc58b6460caaefd02b0c816d2a
Surya B <58516233+surya-b21@users.noreply.github.com>
2023-06-19 21:45:59 +0700

    seeder

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'sku' => rand(1000000, 9999999),
+                'nama' => 'Kaca',
+                'harga' => 100000,
+                'stok' => 1000
+            ],
+            [
+                'sku' => rand(1000000, 9999999),
+                'nama' => 'Kran Besi',
+                'harga' => 150000,
+                'stok' => 1000
+            ],
+        ];
+
+        DB::table('items')->insert($data);
+    }
+}
